4afcbfceb3521c85740bd56471209aae2ded04ad
Dominic Lowes <chaiskeng@gmail.com>
2022-10-16 19:33:30 +0900

    feat: add RestaurantRepository interface

---
+++ src/main/java/restaurant/RestaurantRepository.java
@@ -0,0 +1,8 @@
+package restaurant;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends MongoRepository<Restaurant, String> {
+}
