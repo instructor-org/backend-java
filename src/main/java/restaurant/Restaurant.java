5be6eb5e4a8dcfc915259854370010de6869458b
Dominic Lowes <chaiskeng@gmail.com>
2022-10-18 16:57:46 +0900

    refactor: update restaurant class

--- src/main/java/restaurant/Restaurant.java
+++ src/main/java/restaurant/Restaurant.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.annotation.Generated;
+import java.util.List;
 
 @Data
 @Document(collection = "restaurants")
@@ -14,10 +14,20 @@ public class Restaurant {
     private String name;
     private String cuisine;
     private String address;
+    private int rating;
+    private List<String> opening_hours;
+    private String url;
+    private List<Object> reviews;
+    private String business_status;
 
-    public Restaurant(String name, String cuisine, String address) {
+    public Restaurant(String name, String cuisine, String address, int rating, List<String> opening_hours, String url, List<Object> reviews, String business_status) {
         this.name = name;
         this.cuisine = cuisine;
         this.address = address;
+        this.rating = rating;
+        this.opening_hours = opening_hours;
+        this.url = url;
+        this.reviews = reviews;
+        this.business_status = business_status;
     }
 }
