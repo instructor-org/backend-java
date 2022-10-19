f565da83477760ec89aa37d6110879b6ee1be226
Dominic Lowes <chaiskeng@gmail.com>
2022-10-19 15:17:50 +0900

    refactor: changed method name in restaurant service

--- src/main/java/com/foodapp/restaurant/RestaurantService.java
+++ src/main/java/com/foodapp/restaurant/RestaurantService.java
@@ -1,13 +1,11 @@
-package restaurant;
+package com.foodapp.restaurant;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
 @Service
-@AllArgsConstructor
 public class RestaurantService {
 
     private final RestaurantRepository repository;
