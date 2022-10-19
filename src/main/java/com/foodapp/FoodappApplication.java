f0336c18e47d67bcb39eda055cb8efabb3d6643b
Dominic Lowes <chaiskeng@gmail.com>
2022-10-19 15:24:26 +0900

    feat: add command line runner to populate db for testing

--- src/main/java/com/foodapp/FoodappApplication.java
+++ src/main/java/com/foodapp/FoodappApplication.java
@@ -1,13 +1,28 @@
 package com.foodapp;
 
+import com.foodapp.restaurant.Restaurant;
+import com.foodapp.restaurant.RestaurantRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
 
 @SpringBootApplication
 public class FoodappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodappApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner(RestaurantRepository repo) {
+		return args -> {
+			Restaurant restaurant = new Restaurant("joe", "monroe","joemon@joe", 1,  Arrays.asList("opening_hours"), "hello",Arrays.asList("reviews"), "closed");
+				repo.insert(restaurant);
+		};
 	}
 
 }
