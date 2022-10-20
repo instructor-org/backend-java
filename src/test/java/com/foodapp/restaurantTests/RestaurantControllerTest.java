e76129d6cf42ede82d8a5bff6c684aa22789a28f
Dominic Lowes <chaiskeng@gmail.com>
2022-10-20 14:59:46 +0900

    feat: ensure restaurant controller get method returns correct status

--- src/test/java/com/foodapp/restaurantTests/RestaurantControllerTest.java
+++ src/test/java/com/foodapp/restaurantTests/RestaurantControllerTest.java
@@ -1,17 +1,21 @@
-package restaurantTests;
+package com.foodapp.restaurantTests;
 
+import com.foodapp.restaurant.Restaurant;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.annotation.RestController;
 import com.foodapp.restaurant.RestaurantService;
+
+import java.util.Arrays;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.web.servlet.function.RequestPredicates.contentType;
 
 @AutoConfigureMockMvc
 @SpringBootTest()
@@ -19,8 +23,8 @@ class RestaurantControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-//    @MockBean
-//    private RestaurantService restaurantService;
+    @MockBean
+    private RestaurantService restaurantService;
     @Test
     public void test_get_all_restaurants() throws Exception {
         this.mockMvc.perform(get("/food/restaurant"))
@@ -30,7 +34,9 @@ class RestaurantControllerTest {
     @Test
     void saveRestaurant() {
     }
-
+    @Test
+    void whenValidInput_returnsStatus200() throws Exception {
+    }
 
 
     @Test
--- src/test/java/com/foodapp/restaurantTests/RestaurantServiceTest.java
+++ src/test/java/com/foodapp/restaurantTests/RestaurantServiceTest.java
@@ -1,4 +1,4 @@
-package restaurantTests;
+package com.foodapp.restaurantTests;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
