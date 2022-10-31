6b67687d86baccd13ec23a7b032be78a848a9878
Dominic Lowes <chaiskeng@gmail.com>
2022-10-31 15:45:41 +0900

    feat: add routing for finding restaurant by cuisine

--- src/main/java/com/foodapp/restaurant/RestaurantController.java
+++ src/main/java/com/foodapp/restaurant/RestaurantController.java
@@ -1,5 +1,6 @@
 package com.foodapp.restaurant;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
@@ -18,14 +19,13 @@ public class RestaurantController {
         this.repository = repository;
     }
 
-
     @PostMapping("/restaurant")
     public String saveRestaurant(@RequestBody Restaurant restaurant) {
         repository.save(restaurant);
         return "Restaurant created successfully!";
     }
 
-    @GetMapping("restaurant")
+    @GetMapping("/restaurant")
     public List<Restaurant> getAllRestaurants() {
         return restaurantService.getAllRestaurants();
     }
@@ -33,6 +33,10 @@ public class RestaurantController {
     @GetMapping("/restaurant/{id}")
     public Optional<Restaurant> getRestaurantById(@PathVariable String id) {
         return restaurantService.getRestaurantById(id);
+    }
+    @GetMapping("/restaurant/cuisine/{cuisine}")
+    public List<Restaurant> getRestaurantsByCuisine(@PathVariable String cuisine) {
+        return restaurantService.getRestaurantByCuisine(cuisine);
     }
 
     @PutMapping("/restaurant/{id}")
