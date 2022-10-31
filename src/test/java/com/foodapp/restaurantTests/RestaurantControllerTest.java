d18d8d83c8b0a2f616e935eb8b23e79cbe3e86ee
Dominic Lowes <chaiskeng@gmail.com>
2022-10-31 09:47:35 +0900

    feat: create static method to convert test restaurant into json and create POST route test

--- src/test/java/com/foodapp/restaurantTests/RestaurantControllerTest.java
+++ src/test/java/com/foodapp/restaurantTests/RestaurantControllerTest.java
@@ -1,21 +1,22 @@
 package com.foodapp.restaurantTests;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.foodapp.restaurant.Restaurant;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import com.foodapp.restaurant.RestaurantService;
 
+
 import java.util.Arrays;
 
+import static org.springframework.data.mongodb.util.BsonUtils.toJson;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.web.servlet.function.RequestPredicates.contentType;
+
 
 @AutoConfigureMockMvc
 @SpringBootTest()
@@ -26,13 +27,19 @@ class RestaurantControllerTest {
     @MockBean
     private RestaurantService restaurantService;
     @Test
-    public void test_get_all_restaurants() throws Exception {
-        this.mockMvc.perform(get("/food/restaurant"))
+    void test_get_all_restaurants() throws Exception {
+        this.mockMvc.perform(get("/food/restaurant")
+                .contentType("application/json"))
                 .andExpect(status().isOk());
 
     }
     @Test
-    void saveRestaurant() {
+    void saveRestaurant() throws Exception {
+        Restaurant testaurant = new Restaurant("Hubert's Eggs", "Hawaiian", "46 Wallaby Way, Sydney", 5, Arrays.asList("Opening hours"), "url", Arrays.asList("reviews"), "Open");
+        this.mockMvc.perform(post("/food/restaurant")
+                        .content(asJson(testaurant))
+                        .contentType("application/json"))
+                        .andExpect(status().isOk());
     }
     @Test
     void whenValidInput_returnsStatus200() throws Exception {
@@ -49,5 +56,13 @@ class RestaurantControllerTest {
 
     @Test
     void deleteRestaurant() {
+    }
+
+    public static String asJson(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
     }
 }
